c200bc1d0ffcbd34373dc494884c362f50772b48
DanilaPatsiora <dpatsora@gmail.com>
2019-09-26 12:49:52 +0300

    Feature: Switch from geth to Parity (#25)
    * Use Parity instead of Geth
    
    * Fix gpg keyserver URL
    
    * Fix Parity config

--- bin/install.sh
+++ bin/install.sh
@@ -29,7 +29,7 @@ EOS
 
 install_ruby() {
   sudo -u app bash <<EOS
-  gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable
 EOS
 
