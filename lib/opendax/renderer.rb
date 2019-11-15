9caa3032d0ac744e19d1fbbb3feee5f98cc43a04
DanilaPatsiora <dpatsora@gmail.com>
2019-11-15 22:17:43 +0200

    Feature: add render_protect field to app.yml (#39)

--- lib/opendax/renderer.rb
+++ lib/opendax/renderer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'openssl'
 require 'sshkey'
 require 'pathname'
@@ -9,20 +11,11 @@ module Opendax
   class Renderer
     TEMPLATE_PATH = Pathname.new('./templates')
 
-    BARONG_KEY = 'config/secrets/barong.key'.freeze
-    APPLOGIC_KEY = 'config/secrets/applogic.key'.freeze
-    SSH_KEY = 'config/secrets/app.key'.freeze
+    BARONG_KEY = 'config/secrets/barong.key'
+    APPLOGIC_KEY = 'config/secrets/applogic.key'
+    SSH_KEY = 'config/secrets/app.key'
 
     def render
-      Dir.glob("#{TEMPLATE_PATH}/**/*.erb").each do |file|
-        output_file = template_name(file)
-        render_file(file, output_file)
-      end
-    end
-
-    def render_file(file, out_file)
-      puts "Rendering #{out_file}"
-
       @config ||= config
       @utils  ||= utils
       @barong_key ||= OpenSSL::PKey::RSA.new(File.read(BARONG_KEY), '')
@@ -32,7 +25,17 @@ module Opendax
       @applogic_private_key ||= Base64.urlsafe_encode64(@applogic_key.to_pem)
       @applogic_public_key ||= Base64.urlsafe_encode64(@applogic_key.public_key.to_pem)
 
-      result = ERB.new(File.read(file), 0, '-').result(binding)
+      Dir.glob("#{TEMPLATE_PATH}/**/*.erb").each do |file|
+        output_file = template_name(file)
+        FileUtils.chmod 0o644, output_file if File.exist?(output_file)
+        render_file(file, output_file)
+        FileUtils.chmod 0o444, output_file if @config['render_protect']
+      end
+    end
+
+    def render_file(file, out_file)
+      puts "Rendering #{out_file}"
+      result = ERB.new(File.read(file), trim_mode: '-').result(binding)
       File.write(out_file, result)
     end
 
@@ -57,7 +60,9 @@ module Opendax
       unless File.file?(filename)
         key = SSHKey.generate(type: 'RSA', bits: 2048)
         File.open(filename, 'w') { |file| file.puts(key.private_key) }
-        File.open("#{filename}.pub", 'w') { |file| file.puts(key.ssh_public_key) } if public
+        if public
+          File.open("#{filename}.pub", 'w') { |file| file.puts(key.ssh_public_key) }
+        end
       end
     end
 
--- spec/opendax/renderer_spec.rb
+++ spec/opendax/renderer_spec.rb
@@ -9,6 +9,7 @@ describe Opendax::Renderer do
         'name' => 'Opendax',
         'domain' => 'app.test'
       },
+      'render_protect' => false,
       'ssl' => {
         'enabled' => 'true',
         'email' => 'support@example.com'
@@ -61,7 +62,6 @@ describe Opendax::Renderer do
 
   describe '.render_file' do
     it 'should render file' do
-      expect(renderer).to receive(:config).once
       expect(ERB).to receive(:new).once.and_call_original
       expect_any_instance_of(ERB).to receive(:result).once.and_return(fake_erb_result)
       expect(File).to receive(:write).with('./config/peatio.env', fake_erb_result).once.and_call_original
@@ -87,13 +87,9 @@ describe Opendax::Renderer do
   describe '.render' do
     it 'should call exact amount of helper functions' do
       number_of_files = Dir.glob('./templates/**/*.erb').length
-
-      expect(renderer).to receive(:render_keys).once
       expect(renderer).to receive(:render_file).exactly(number_of_files).times
-      expect(renderer).to receive(:template_name).exactly(number_of_files).times
 
       renderer.render
-      renderer.render_keys
     end
   end
 end
