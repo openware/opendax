b129295d3fa15c1928112b17bb49118facb316d2
Danylo <dpatsora@gmail.com>
2020-01-06 16:51:34 +0200

    Change ssh user to deploy

--- bin/install.sh
+++ bin/install.sh
@@ -36,13 +36,13 @@ EOS
 }
 
 activate_gcloud() {
-  sudo -u app bash <<EOS
+  sudo -u deploy bash <<EOS
   gcloud auth configure-docker --quiet
 EOS
 }
 
 install_ruby() {
-  sudo -u app bash <<EOS
+  sudo -u deploy bash <<EOS
   gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable
 EOS
