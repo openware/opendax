b1bc2f25e6abac9cc8d5483788838d35f71338d3
Valentine Shatravenko <vshatravenko@heliostech.fr>
2021-02-24 15:50:19 +0200

    Enhancement: Integrate kaigara & sonic Vault changes

--- lib/opendax/vault.rb
+++ lib/opendax/vault.rb
@@ -2,7 +2,7 @@
 
 module Opendax
   class Vault
-    POLICIES_NAMES = ["barong", "finex_engine", "peatio_rails", "peatio_crypto", "peatio_upstream", "peatio_matching"]
+    POLICIES_NAMES = ["barong", "finex_engine", "peatio_rails", "peatio_crypto", "peatio_upstream", "peatio_matching", "kaigara", "sonic"]
 
     def vault_secrets_path
       'config/vault-secrets.yml'
