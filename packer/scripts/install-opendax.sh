558afb50a3d274c885a8c7868b8a158032bc70b6
Valentine Shatravenko <vshatravenko@heliostech.fr>
2021-02-26 00:02:24 +0200

    Fix the Compose timeout error

--- packer/scripts/install-opendax.sh
+++ packer/scripts/install-opendax.sh
@@ -3,6 +3,9 @@
 sudo -u deploy bash <<EOS
   source /home/deploy/.rvm/scripts/rvm
 
+  export DOCKER_CLIENT_TIMEOUT=120
+  export COMPOSE_HTTP_TIMEOUT=120
+
   cd /home/deploy/opendax
   gem install bundler
   bundle install
