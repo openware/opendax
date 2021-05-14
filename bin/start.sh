53a3bd519f9f179d42ec28696ff9045b0d8a3e2e
Andrew Peresada <kohelbekker@gmail.com>
2021-05-14 11:22:01 +0300

    Feature: Add barong envs for upload size range (#326)
    * Delete duplicated parameters (#327)
    * Update start.sh and secrets.yaml

--- bin/start.sh
+++ bin/start.sh
@@ -15,16 +15,11 @@ install_opendax() {
   cd opendax
 
   bundle install
-  rake render:config
-  rake service:cryptonodes && \
-  until rake wallet:create['deposit','http://127.0.0.1:8545','changeme']; do sleep 15; done && \
-  rake wallet:create['hot','http://127.0.0.1:8545','changeme'] && \
-  rake wallet:create['warm','http://127.0.0.1:8545','changeme'] && \
   rake render:config && \
   rake service:all && \
   rake service:daemons && \
   chmod +x bin/install_webhook
-  ./bin/install_webhook
+  sudo ./bin/install_webhook
 EOS
 }
 
